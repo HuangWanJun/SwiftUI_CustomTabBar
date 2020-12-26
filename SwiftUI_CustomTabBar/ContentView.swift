6638c501f124385390b779ed009e0fa1c60ae7ed
Samuel Huang <samuel.huang@tonghaifinancial.com>
2020-12-26 17:11:39 +0800

    Initial Commit

---
+++ SwiftUI_CustomTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_CustomTabBar
+//
+//  Created by Samuel Huang on 26/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_CustomTabBar/SwiftUI_CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CustomTabBarApp.swift
+//  SwiftUI_CustomTabBar
+//
+//  Created by Samuel Huang on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
